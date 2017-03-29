ecfa6ba6e36eaa3963f4a3b9061c950b436a6d1c
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-03-29 22:27:05 +0100

    Spring Tutorial 40 - Configuring connection Pooling with Apache DBCP

---
+++ spring-tutorial-40_ConnectionPoolingWithApacheDBCP/readme.txt
@@ -0,0 +1,5 @@
+This project is showing:
+
+- How to setup the project to use annotations.
+- How to use the autowired annotation.
+- How to use the required attribute.
---
+++ spring-tutorial-40_ConnectionPoolingWithApacheDBCP/src/main/java/com/caveofprogramming/test/OffersDAO.java
@@ -0,0 +1,11 @@
+package com.caveofprogramming.test;
+
+import java.util.List;
+
+public class OffersDAO {
+
+	public List<Offer> getOffers(){
+		return null;
+	}
+	
+}
