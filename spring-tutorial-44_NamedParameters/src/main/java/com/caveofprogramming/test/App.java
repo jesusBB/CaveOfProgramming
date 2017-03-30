2a42ae57810534ffa614d8b8442d85c7d48c27ae
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-03-30 20:29:45 +0100

    Spring tutorial 44 - Using NamedParameterJDBCTemplate to add dynamic parameters to the select statements

---
+++ spring-tutorial-44_NamedParameters/src/main/java/com/caveofprogramming/test/App.java
@@ -0,0 +1,40 @@
+package com.caveofprogramming.test;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+
+public class App {
+
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/test/beans/beans.xml");
+		
+		OffersDAO offersDAO = (OffersDAO) context.getBean("offersDao");
+		
+		List<Offer> offers;
+		try {
+			offers = offersDAO.getOffers();
+			for(Offer offer : offers){
+				System.out.println(offer);
+			}
+			
+			Offer offer = offersDAO.getOffer(2);
+			System.out.println(offer);
+		} catch (CannotGetJdbcConnectionException ex){
+			System.out.println("Cannot get database connection");
+		}
+		 catch (DataAccessException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass());
+		}
+		
+		((ClassPathXmlApplicationContext)context).close();
+		
+	}
+
+}
