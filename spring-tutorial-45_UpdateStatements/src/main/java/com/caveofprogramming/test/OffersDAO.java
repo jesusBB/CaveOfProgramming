33c552b94147f7adfc40884745da8bbd2d129389
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-04-01 11:27:32 +0100

    Spring Tutorial 45 - Adding delete method to DAO

---
+++ spring-tutorial-45_UpdateStatements/src/main/java/com/caveofprogramming/test/OffersDAO.java
@@ -0,0 +1,66 @@
+package com.caveofprogramming.test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component("offersDao")
+public class OffersDAO {
+
+	private NamedParameterJdbcTemplate jdbc;
+	
+	@Autowired
+	public void setDataSource(DataSource jdbc) {
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+	}
+
+	public List<Offer> getOffers(){
+		
+		return jdbc.query("select * from offers ", new MapSqlParameterSource() , new RowMapper<Offer>(){
+
+			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Offer offer = new Offer();
+				offer.setId(rs.getInt("id"));
+				offer.setName(rs.getString("name"));
+				offer.setEmail(rs.getString("email"));
+				offer.setText(rs.getString("text"));
+				return offer;
+			}
+			
+		});
+	}
+	
+	public Offer getOffer(int id){
+		
+		MapSqlParameterSource params = new MapSqlParameterSource(/*"nameParam", "Bob"*/);
+		params.addValue("idParam", id);
+		
+		return jdbc.queryForObject("select * from offers where id = :idParam", params, new RowMapper<Offer>(){
+
+			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Offer offer = new Offer();
+				offer.setId(rs.getInt("id"));
+				offer.setName(rs.getString("name"));
+				offer.setEmail(rs.getString("email"));
+				offer.setText(rs.getString("text"));
+				return offer;
+			}
+			
+		});
+	}
+	
+	public boolean delete(int id){
+		MapSqlParameterSource params = new MapSqlParameterSource("idParam", id);
+		return jdbc.update("delete from offers where id= :idParam", params) == 1;
+	}
+	
+}
