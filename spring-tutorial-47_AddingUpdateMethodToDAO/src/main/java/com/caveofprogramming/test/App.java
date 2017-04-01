1041a47c268c5e9c686b8d7297d0295d0d1126ea
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-04-01 12:01:24 +0100

    Spring Tutorial 47 - Adding update method to DAO

---
+++ spring-tutorial-47_AddingUpdateMethodToDAO/src/main/java/com/caveofprogramming/test/App.java
@@ -0,0 +1,60 @@
+package com.caveofprogramming.test;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+
+public class App {
+
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/test/beans/beans.xml");
+		
+		OffersDAO offersDAO = (OffersDAO) context.getBean("offersDao");
+		
+		List<Offer> offers;
+		try {
+			
+			Offer offer2 = new Offer(7, "Claire", "Claire@caveofprogramming.com", "Web design to fit any budget");
+			if(offersDAO.update(offer2)){
+				System.out.println("Object updated");
+			}else{
+				System.out.println("Cannot update the object");
+			}
+			
+//			Offer offer2 = new Offer("Karen", "karen@caveofprogramming.com", "Software testing");
+				
+			/*if(offersDAO.create(offer1)){
+				System.out.println("Object created");
+			}
+			
+			if(offersDAO.create(offer2)){
+				System.out.println("Object created");
+			}
+			
+			offers = offersDAO.getOffers();
+			for(Offer offer : offers){
+				System.out.println(offer);
+			}*/
+			
+			Offer offer = offersDAO.getOffer(2);
+			System.out.println(offer);
+			
+			System.out.println("Rows deleted: " + (offersDAO.delete(2) ? 1 : 0));
+		} catch (CannotGetJdbcConnectionException ex){
+			System.out.println("Cannot get database connection");
+		}
+		 catch (DataAccessException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass());
+		}
+		
+		((ClassPathXmlApplicationContext)context).close();
+		
+	}
+
+}
