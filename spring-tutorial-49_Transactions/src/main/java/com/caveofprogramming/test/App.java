c27a97e74ddee6d01c6c361f3703f8e02daabdeb
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-04-02 02:13:33 +0100

    Spring Tutorial 49 - Transactions

---
+++ spring-tutorial-49_Transactions/src/main/java/com/caveofprogramming/test/App.java
@@ -0,0 +1,55 @@
+package com.caveofprogramming.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+
+public class App {
+
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/test/beans/beans.xml");
+		
+		OffersDAO offersDAO = (OffersDAO) context.getBean("offersDao");
+		
+		List<Offer> offers = new ArrayList<Offer>();
+		try {
+			
+			Offer offer2 = new Offer(7, "Claire", "Claire@caveofprogramming.com", "Web design to fit any budget");
+			if(offersDAO.update(offer2)){
+				System.out.println("Object updated");
+			}else{
+				System.out.println("Cannot update the object");
+			}
+			
+			offers.add(offer2);
+			offers.add(new Offer("Jesuli", "Jesuli@caveofprogramming.com", "Cash for Software"));
+			offers.add(new Offer("Paco", "Paco@caveofprogramming.com", "Elegant web design"));
+			int[] rivals = offersDAO.create(offers);
+			
+			for(int value : rivals){
+				System.out.println("updated " + value + " rows.");
+			}
+			
+//			Offer offer = offersDAO.getOffer(2);
+//			System.out.println(offer);
+			
+//			System.out.println("Rows deleted: " + (offersDAO.delete(2) ? 1 : 0));
+		} catch (CannotGetJdbcConnectionException ex){
+			System.out.println("Cannot get database connection");
+		}
+		 catch (DataAccessException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass());
+		}
+		
+		((ClassPathXmlApplicationContext)context).close();
+		
+	}
+
+}
---
+++ spring-tutorial-49_Transactions/src/main/java/com/caveofprogramming/test/OffersDAO.java
@@ -0,0 +1,86 @@
+package com.caveofprogramming.test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
+import org.springframework.stereotype.Component;
+
+@Component("offersDao")
+public class OffersDAO {
+
+	private NamedParameterJdbcTemplate jdbc;
+	
+	@Autowired
+	public void setDataSource(DataSource jdbc) {
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+	}
+
+	public List<Offer> getOffers(){
+		
+		return jdbc.query("select * from offers ", new MapSqlParameterSource() , new RowMapper<Offer>(){
+
+			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Offer offer = new Offer();
+				offer.setId(rs.getInt("id"));
+				offer.setName(rs.getString("name"));
+				offer.setEmail(rs.getString("email"));
+				offer.setText(rs.getString("text"));
+				return offer;
+			}
+			
+		});
+	}
+	
+	public Offer getOffer(int id){
+		
+		MapSqlParameterSource params = new MapSqlParameterSource(/*"nameParam", "Bob"*/);
+		params.addValue("idParam", id);
+		
+		return jdbc.queryForObject("select * from offers where id = :idParam", params, new RowMapper<Offer>(){
+
+			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Offer offer = new Offer();
+				offer.setId(rs.getInt("id"));
+				offer.setName(rs.getString("name"));
+				offer.setEmail(rs.getString("email"));
+				offer.setText(rs.getString("text"));
+				return offer;
+			}
+			
+		});
+	}
+	
+	public boolean create(Offer offer){
+		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(offer);
+		
+		return jdbc.update("insert into offers (name, text, email) values (:name, :text, :email)", params) == 1;
+	}
+	
+	public int[] create(List<Offer> offers){
+		SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(offers.toArray());
+		return jdbc.batchUpdate("insert into offers (name, text, email) values (:name, :text, :email)", params);
+		
+	}
+
+	public boolean delete(int id){
+		MapSqlParameterSource params = new MapSqlParameterSource("idParam", id);
+		return jdbc.update("delete from offers where id= :idParam", params) == 1;
+	}
+	
+	public boolean update(Offer offer){
+		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(offer);
+		
+		return jdbc.update("update offers set name =:name, text = :text, email = :email where id = :id", params) == 1;
+	}
+	
+}
