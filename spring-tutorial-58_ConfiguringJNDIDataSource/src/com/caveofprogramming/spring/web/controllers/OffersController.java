36cd31e40f107e2757b26575163fed7432cc581b
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-04-04 22:08:25 +0100

    spring tutorial 58 - Setting up JNDI Datasource and test the connection inside the jsp. Modify context.xml of tomcat to configurate the Datasource

---
+++ spring-tutorial-58_ConfiguringJNDIDataSource/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -0,0 +1,32 @@
+package com.caveofprogramming.spring.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OffersController {
+	
+	/*
+	@RequestMapping("/")
+	public ModelAndView showHome() {
+		
+		ModelAndView mv = new ModelAndView("home");
+		
+		Map<String, Object> model = mv.getModel();
+		
+		model.put("name", "<b>River</b>");
+		
+		return mv;
+	}
+	*/
+	
+	@RequestMapping("/")
+	public String showHome(Model model) {
+		
+		model.addAttribute("name", "Tiffany");
+		
+		return "home2";
+	}
+}
+
