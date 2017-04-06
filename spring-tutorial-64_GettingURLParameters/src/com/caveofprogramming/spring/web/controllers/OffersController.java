5e1125e3f48dbe67568a6fad2dae90940e676642
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-04-06 19:40:51 +0100

    Getting values in the controller from the form. Adding some css to the jsp file

--- spring-tutorial-64_GettingURLParameters/src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ spring-tutorial-64_GettingURLParameters/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -45,5 +45,12 @@ public class OffersController {
 				
 		return "createoffer";
 	}
+	
+	@RequestMapping(value="/docreate", method=RequestMethod.POST)
+	public String doCreate(Model model, Offer offer) {
+		System.out.println(offer);
+		return "offercreated";
+	}
+
 }
 
