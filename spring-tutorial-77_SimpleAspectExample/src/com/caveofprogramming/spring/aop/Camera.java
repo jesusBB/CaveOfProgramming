d2bd933cf1a79c648141c80bc742eb4222b3c08f
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-04-11 21:47:10 +0100

    Spring tutorial 77 - Simple Aspect example

---
+++ spring-tutorial-77_SimpleAspectExample/src/com/caveofprogramming/spring/aop/Camera.java
@@ -0,0 +1,8 @@
+package com.caveofprogramming.spring.aop;
+
+//Advice
+public class Camera {
+	public void snap(){
+		System.out.println("SNAP!");
+	}
+}
---
+++ spring-tutorial-77_SimpleAspectExample/src/com/caveofprogramming/spring/aop/Logger.java
@@ -0,0 +1,8 @@
+package com.caveofprogramming.spring.aop;
+
+//Aspect
+public class Logger {
+	public void aboutToTakeAPhoto(){
+		System.out.println("About to take a photo ...");
+	}
+}
