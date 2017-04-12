5469e5c885642bf057c01e60aaab7ceb506f52b5
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-04-13 00:01:41 +0100

    Spring tutorial 81 - Explanation about proxies in Aspects. Creation of interfaces to avoid to call twice to the constructor of a class when is being instantiated. It's called twice: one for the proxy subclass and one for the ojbect itself.

---
+++ spring-tutorial-81_ProxiesInterfacesAndAspects/src/com/caveofprogramming/spring/aop/ICamera.java
@@ -0,0 +1,13 @@
+package com.caveofprogramming.spring.aop;
+
+public interface ICamera {
+
+	void snap() throws Exception;
+
+	void snap(int exposure);
+
+	String snap(String name);
+
+	void snapNightime();
+
+}
