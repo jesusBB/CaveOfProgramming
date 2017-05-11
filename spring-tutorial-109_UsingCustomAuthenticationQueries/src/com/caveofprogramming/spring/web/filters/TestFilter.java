baf632c689d5273e3fe0545ade91f5a3ff78eb96
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-05-11 22:05:24 +0100

    Spring tutorial 110 - Using custom queries to run case sensitive select statements for the users table and the authority.

---
+++ spring-tutorial-109_UsingCustomAuthenticationQueries/src/com/caveofprogramming/spring/web/filters/TestFilter.java
@@ -0,0 +1,55 @@
+package com.caveofprogramming.spring.web.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class TestFilter
+ */
+public class TestFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public TestFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	
+	
+	//This method will be executed when the URL-mapping specified in the web.xml matches with this filter.
+	//When chain.doFilter is executed the next step of the process will be run, in this case will be the DispatcherServlet
+	//of Spring which is the Servlet executed by default for all the request in this application
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		System.out.println("AAAAAAAAAAAAAAAA: " + ((HttpServletRequest)request).getRequestURI());
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
