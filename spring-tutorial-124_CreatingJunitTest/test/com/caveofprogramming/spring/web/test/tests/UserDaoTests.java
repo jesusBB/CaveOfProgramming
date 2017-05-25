e14bc7585801702981ee6ed045383dcc10afd6ac
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-05-25 15:42:44 +0100

    Spring Tutorial 124 - Creating JUnit test. Added new structure of packages for testing. Added new datasource.xml file for "dev" profile pointing to test DB. Added new jdbc.properties file with test url DB Added new JUnit class for testing on test environment

---
+++ spring-tutorial-124_CreatingJunitTest/test/com/caveofprogramming/spring/web/test/tests/UserDaoTests.java
@@ -0,0 +1,22 @@
+package com.caveofprogramming.spring.web.test.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ActiveProfiles("dev")
+//very important to use the datasource.xml from the test package and "dev" profile
+@ContextConfiguration(locations = { "classpath:com/caveofprogramming/spring/web/config/dao-context.xml",
+		"classpath:com/caveofprogramming/spring/web/config/security-context.xml",
+		"classpath:com/caveofprogramming/spring/web/test/config/datasource.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserDaoTests {
+	@Test
+	public void testCreateUser(){
+		assertEquals("Dummy test",1,1);
+	}
+}
