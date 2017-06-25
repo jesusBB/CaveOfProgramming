1776bda1b1adf927b1e9ab9f4c72a9adc6114868
Jesus Bustillo Barrios <jesus_bustillo@yahoo.es>
2017-06-25 20:42:56 +0100

    Spring tutorial 160 - Adding jackson dependencies and creating a new method in controller to return a list of messages in JSON format

---
+++ spring-tutorial-160_CreatingJsonServer/test/com/caveofprogramming/spring/web/test/tests/MessageDaoTests.java
@@ -0,0 +1,84 @@
+package com.caveofprogramming.spring.web.test.tests;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.caveofprogramming.spring.web.dao.Message;
+import com.caveofprogramming.spring.web.dao.MessagesDao;
+import com.caveofprogramming.spring.web.dao.Offer;
+import com.caveofprogramming.spring.web.dao.OffersDao;
+import com.caveofprogramming.spring.web.dao.User;
+import com.caveofprogramming.spring.web.dao.UsersDao;
+
+@ActiveProfiles("dev")
+@ContextConfiguration(locations = {
+		"classpath:com/caveofprogramming/spring/web/config/dao-context.xml",
+		"classpath:com/caveofprogramming/spring/web/config/security-context.xml",
+		"classpath:com/caveofprogramming/spring/web/test/config/datasource.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class MessageDaoTests {
+	
+
+
+	@Autowired
+	private OffersDao offersDao;
+
+	@Autowired
+	private UsersDao usersDao;
+
+	@Autowired
+	private MessagesDao messagesDao;
+
+	@Autowired
+	private DataSource dataSource;
+	
+	private User user1 = new User("johnwpurcell", "John Purcell", "hellothere",
+			"john@caveofprogramming.com", true, "ROLE_USER");
+	private User user2 = new User("richardhannay", "Richard Hannay", "the39steps",
+			"richard@caveofprogramming.com", true, "ROLE_ADMIN");
+	private User user3 = new User("suetheviolinist", "Sue Black", "iloveviolins",
+			"sue@caveofprogramming.com", true, "ROLE_USER");
+	private User user4 = new User("rogerblake", "Rog Blake", "liberator",
+			"rog@caveofprogramming.com", false, "user");
+
+
+	private Offer offer1 = new Offer(user1, "This is a test offer.");
+	private Offer offer2 = new Offer(user1, "This is another test offer.");
+	private Offer offer3 = new Offer(user2, "This is yet another test offer.");
+	private Offer offer4 = new Offer(user3, "This is a test offer once again.");
+	private Offer offer5 = new Offer(user3, "Here is an interesting offer of some kind.");
+	private Offer offer6 = new Offer(user3, "This is just a test offer.");
+	private Offer offer7 = new Offer(user4, "This is a test offer for a user that is not enabled.");
+
+	@Before
+	public void init() {
+		JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+
+		jdbc.execute("delete from offers");
+		jdbc.execute("delete from messages");
+		jdbc.execute("delete from users");
+	}
+	
+	@Test
+	public void testSave() {
+		usersDao.create(user1);
+		usersDao.create(user2);
+		usersDao.create(user3);
+		usersDao.create(user4);
+		
+		Message message1 = new Message("Test subject 1", "Test content 1", "Isaac Neweton", "isaac@gmail.com", user1.getUsername());
+		messagesDao.saveOrUpdate(message1);
+		
+	}
+	
+	
+
+}
